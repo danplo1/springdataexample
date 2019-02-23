6bda3c42035db3f39db6c68188ce6461760970ed
danplo1 <budweiser401@wp.pl>
2019-02-23 21:06:35 +0100

    create Start class, test and redirection of data to the new database

--- src/main/java/pl/danplo/springdataexample/Start.java
+++ src/main/java/pl/danplo/springdataexample/Start.java
@@ -18,17 +18,17 @@ public class Start {
 
     @EventListener(ApplicationReadyEvent.class)
     public void runExample() {
-//        Car car1 = new Car("Volvo", "XC90", Color.SILVER);
-//        carRepo.save(car1);
-//
-//        Car car2 = new Car("Audi", "Q7", Color.WHITE);
-//        carRepo.save(car2);
-//
-//        Car car3 = new Car("BMW", "X5", Color.BLACK);
-//        carRepo.save(car3);
-//
-//        Car car4 = new Car("Toyota", "RAW", Color.RED);
-//        carRepo.save(car4);
+        Car car1 = new Car("Volvo", "XC90", Color.SILVER);
+        carRepo.save(car1);
+
+        Car car2 = new Car("Audi", "Q7", Color.WHITE);
+        carRepo.save(car2);
+
+        Car car3 = new Car("BMW", "X5", Color.BLACK);
+        carRepo.save(car3);
+
+        Car car4 = new Car("Toyota", "RAW", Color.RED);
+        carRepo.save(car4);
 
 //        carRepo.deleteById(1L);
 
